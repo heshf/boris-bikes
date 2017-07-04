62c571fab9bc02d99a6126c8c6003daa5cb1ee8f
Hesham Farag <heshamf@gmail.com>
2017-07-04 15:21:57 +0100

    Step8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require './lib/docking_station.rb'
 
-describe DockingStation do
+# describe DockingStation do
+# 	it 'responds to release_bike' do
+# expect(subject).to respond_to :release_bike
+# 	end
+# end
 
+describe DockingStation do
+	it { is_expected.to respond_to :release_bike}
 end
